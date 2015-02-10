ce97768d819aa1c6121016e5f14edb4cbcd9081a
Andrew Powers <andrewraganpowers@gmail.com>
2015-02-09 21:26:56 -0800

    Initial commit for flight.py. This encodes data from any flight logger into geojson, which gets passed back to geosync and allows for easy writing to csv, json or standard out. There's still a bit of work to do to get the datetime to work with geojson.

---
+++ flight.py
@@ -0,0 +1,90 @@
+#!/usr/bin/env python
+
+import json
+import os
+
+# order the points by time
+
+class MyEncoder(json.JSONEncoder):
+
+    def default(self, obj):
+        if isinstance(obj, datetime.datetime):
+            return int(mktime(obj.timetuple()))
+
+        return json.JSONEncoder.default(self, obj)
+
+
+class FlightLog(object):
+
+    def __init__(self, points):
+        self.type = "FeatureCollection"
+        self.features = []
+        self.add_points(points)
+
+    def add_points(self, points):
+        for p in points:
+            feature = Feature()
+            feature.geometry.coordinates = [p.longitude, p.latitude, p.altitude]
+            feature.properties = p
+            self.features.append(feature)
+
+    def write(self, filename='STDOUT'):
+
+        output_directory = os.getcwd() # local working directory
+
+        if filename.lower().endswith('.csv'):
+            csvfile  = open(os.path.join(output_directory,filename), "wb")
+            writer = csv.writer(csvfile, delimiter=',')
+            header = ['id','time','latitude','longitude','elevation']
+            # add extra headers if needed
+            #header.extend(extra)
+            writer.writerow(header)
+            idcounter = 0
+            for x in self.features:
+                row = []
+                # add extra data if extra headers where used
+                # row.extend(extra_out)
+                row.append(str(idcounter))
+                row.append(x.properties.date)
+                row.append(x.latitude)
+                row.append(x.longitude)
+                row.append(x.altitude)
+                writer.writerow(row)
+                idcounter=idcounter+1
+            csvfile.flush()
+            csvfile.close()
+
+        elif filename.lower().endswith('.json'):
+            # This does not currently work because it breaks
+            # on the date time stored in feature properties
+            jsonfile = open(os.path.join(output_directory,filename), "wb")
+            json.dump(self, jsonfile, default=lambda o: o.__dict__)
+
+        elif filename == 'STDOUT':
+            print 3
+            header = ['id','time','latitude','longitude','elevation']
+            print ','.join(header)
+            idcounter = 0
+            for x in self.features:
+                row = []
+                row.append(idcounter)
+                row.append(x.properties.date)
+                row.append(x.properties.latitude)
+                row.append(x.properties.longitude)
+                row.append(x.properties.altitude)
+                print ','.join(map(str, row))
+                idcounter=idcounter+1
+
+
+class Feature(object):
+    def __init__(self):
+        self.type = "Feature"
+        self.properties = Store()
+        self.geometry = Store()
+        self.geometry.type = "Point"
+        self.geometry.coordinates = []
+
+
+class Store(object):
+    def __init__(self):
+        pass
